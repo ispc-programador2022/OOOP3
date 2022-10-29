e08d7ae07edca938188d8749af28ca4dfbf7ef29
Marti Octtinger <martiocttinger@gmail.com>
2022-10-29 11:10:54 -0300

    Manejo de errores

--- scraper.py
+++ scraper.py
@@ -7,42 +7,50 @@ class Scraper:
         self.url = url
 
     def parser(self):
-        r = requests.get(self.url)
-        contenido = BeautifulSoup(r.text, 'html.parser')
-        return contenido
+        try:
+            r = requests.get(self.url)
+            contenido = BeautifulSoup(r.text, 'html.parser')
+            return contenido
+        except Exception as e:
+            print(f'Ocurrió un error: {e}')
 
     def extraer(self, contenido):
-        # Traigo todas las tablas (27)
-        jornadas = contenido.find_all('table', class_='jor agendas')
+        try:
+            # Traigo todas las tablas (27)
+            jornadas = contenido.find_all('table', class_='jor agendas')
 
-        # Lista para guardar cada fila
-        filas = []
+            # Lista para guardar cada fila
+            filas = []
 
-        for jornada in jornadas:
-            # Obtengo el nombre de cada jornada
-            jornadaNum = jornada.find('caption').text
+            for jornada in jornadas:
+                # Obtengo el nombre de cada jornada
+                jornadaNum = jornada.find('caption').text
 
-            # Busco el cuerpo de la tabla, donde se encuentra la información que necesitamos, luego obtengo todas las filas
-            for equipos in jornada.find_all('tbody'):
-                rows = equipos.find_all('tr')
+                # Busco el cuerpo de la tabla, donde se encuentra la información que necesitamos, luego obtengo todas las filas
+                for equipos in jornada.find_all('tbody'):
+                    rows = equipos.find_all('tr')
 
-                # Itero cada fila y guardo en variables la información que va a ir en la base de datos
-                for row in rows:
-                    local = row.find('td', class_='local').text.strip()
-                    resultado = row.find('td', class_='resultado').text.strip()
-                    visitante = row.find('td', class_='visitante').text.strip()
+                    # Itero cada fila y guardo en variables la información que va a ir en la base de datos
+                    for row in rows:
+                        local = row.find('td', class_='local').text.strip()
+                        resultado = row.find(
+                            'td', class_='resultado').text.strip()
+                        visitante = row.find(
+                            'td', class_='visitante').text.strip()
 
-                    # Separo el resultado de local y el resultado de visitante
-                    if len(resultado) == 3:
-                        resultadoLocal = resultado[0]
-                        resultadoVisitante = resultado[2]
-                    else:
-                        resultadoLocal = '-'
-                        resultadoVisitante = '-'
+                        # Separo el resultado de local y el resultado de visitante
+                        if len(resultado) == 3:
+                            resultadoLocal = resultado[0]
+                            resultadoVisitante = resultado[2]
+                        else:
+                            resultadoLocal = '-'
+                            resultadoVisitante = '-'
 
-                    filas.append(
-                        Fila(jornadaNum, local, resultadoLocal, visitante, resultadoVisitante))
-        return filas
+                        filas.append(
+                            Fila(jornadaNum, local, resultadoLocal, visitante, resultadoVisitante))
+            return filas
+        except Exception as e:
+            print(f'Ocurrió un error: {e}')
 
 
 class Fila:
